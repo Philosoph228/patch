20dfea23aa84295b34d004cc4481929ad04e3d82
Philosoph228 <philosoph228@gmail.com>
2025-12-12 17:46:20 +0500

    simple_test: update event callback

--- tests/simple_test/main.c
+++ tests/simple_test/main.c
@@ -23,32 +23,47 @@ static const char output_txt_source[] =
     "}\r\n"
     "\r\n";
 
-int test_cbk(char* path, stream_wrapper_t* stream, void* userdata) {
-    if (path == NULL)   /* sane, should not happen */
+int test_cbk(patch_evt_t* evt) {
+    if (evt == NULL) /* Invalid evt */
         return -1;
-    if (userdata == NULL)   /* invalid userdata pointer */
+    if (evt->userdata == NULL) /* invalid userdata pointer */
         return -1;
+    simple_test_data_t* dat = (simple_test_data_t*)evt->userdata;
 
-    printf("Patcher requests path: %s\n", path);
+    if (evt->type == PATCH_EVT_STREAM_ACQUIRE || evt->type == PATCH_EVT_STREAM_RELEASE) {
+        char* path = evt->data.stream_event.path;
+        stream_wrapper_t* sw = evt->data.stream_event.stream;
 
-    simple_test_data_t* dat = (simple_test_data_t*)userdata;
+        if (sw == NULL) /* invalid stream wrapper provided */
+            return -1;
 
-    if (strcmp(path, "input.txt") == 0) {
-        if (dat->input_txt.buf != NULL)
-            dynmem_free(&dat->input_txt);
-        make_dynmem_as_copy(&dat->input_txt, input_txt_source, sizeof(input_txt_source), 1);
-        make_memsw(stream, &dat->input_txt);
-        return 0;
+        printf("Patcher requests path: %s\n", path);
+
+        switch (evt->type) {
+        case PATCH_EVT_STREAM_ACQUIRE: {
+            if (strcmp(path, "input.txt") == 0) {
+                if (dat->input_txt.buf != NULL)
+                    dynmem_free(&dat->input_txt);
+                make_dynmem_as_copy(&dat->input_txt, input_txt_source, sizeof(input_txt_source), 1);
+                make_memsw(sw, &dat->input_txt);
+                return 0;
+            } else if (strcmp(path, "output.txt.tmp") == 0) {
+                if (dat->output_txt.buf != NULL)
+                    dynmem_free(&dat->output_txt);
+                make_dynmem(&dat->output_txt, 0, 0);
+                make_memsw(sw, &dat->output_txt);
+                return 0;
+            }
+            return -1;
+        } break;
+        case PATCH_EVT_STREAM_RELEASE: {
+            /* Do not release for now, just return success */
+            return 0;
+        } break;
+        }
     }
-    else if (strcmp(path, "output.txt.tmp") == 0) {
-        if (dat->output_txt.buf != NULL)
-            dynmem_free(&dat->output_txt);
-        make_dynmem(&dat->output_txt, 0, 0);
-        make_memsw(stream, &dat->output_txt);
-        return 0;
-    }
 
-    return -1;
+    return -1; /* Unknown event, return error */
 }
 
 int main() {
@@ -74,7 +89,7 @@ int main() {
 
     void* patcher = patch_init();
     patch_set_options(patcher, PATCH_OPTION_VERBOSE);
-    patch_set_path_cbk(patcher, (path_cbk_t*)&test_cbk, (void*)&test_data);
+    patch_set_path_cbk(patcher, (patch_event_cbk_t*)&test_cbk, (void*)&test_data);
     apply_patch(patcher, &diff_source_stream);
 
     printf("%s", test_data.output_txt.buf);
